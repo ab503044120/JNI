666eb9eea4fc855cf708680885fc72c677050538
huihui <503044120|@qq.com>
2019-01-30 14:35:52 +0800

    sync

--- CppTest/app/src/main/cpp/native-lib.cpp
+++ CppTest/app/src/main/cpp/native-lib.cpp
@@ -10,60 +10,72 @@
 #include "test/Pet.h"
 
 class base {
- public:
+public:
 
-  base(string &a) {
+    base(string &a) {
 
-  }
+    }
 
-  void a() {
-    __android_log_print(ANDROID_LOG_ERROR, "dog", "base a\n");
-    b(1);
-  };
+    void a() {
+        __android_log_print(ANDROID_LOG_ERROR, "dog", "base a\n");
+        b(1);
+    };
 
-  virtual void b(int x) {
-    __android_log_print(ANDROID_LOG_ERROR, "dog", "base b %d \n", x);
-  };
+    virtual void b(int x) {
+        __android_log_print(ANDROID_LOG_ERROR, "dog", "base b %d \n", x);
+    };
 };
 
 
-class ext: public base {
- public:
-  int bbb;
-  ext(string &a) : base(static_cast<std::string &>("aaa")), bbb(2) {}
+class ext : public base {
+public:
+    int bbb;
 
- private:
-  virtual void b(int x) {
-    __android_log_print(ANDROID_LOG_ERROR, "dog", "ext b %d \n", x);
-  };
+    ext(string &a) : base(static_cast<std::string &>("aaa")), bbb(2) {}
+
+private:
+    virtual void b(int x) {
+        __android_log_print(ANDROID_LOG_ERROR, "dog", "ext b %d \n", x);
+    };
 
 };
 
+int &return0() {
+
+    int a = 0;
+    return a;
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_org_huihui_cpptest_MainActivity_stringFromJNI(
-    JNIEnv *env,
-    jobject /* this */) {
-  std::string hello = "Hello from C++";
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
 
-  Hasky hasky("汪汪");
-  hasky.Pet::call();
-  hasky.haha();
+    Hasky hasky("汪汪");
+    hasky.Pet::call();
+    hasky.haha();
 
-  Animal &a = hasky;
-  a.haha();
-  string aa = "a";
+    Animal &a = hasky;
+    a.haha();
 
-  string &bb = (string &) "a";
+    int &a0 = return0();
 
-  ext ext_i(bb);
-  base base_i(bb);
-  base *base_p;
-  base &base_r = ext_i;
-  base_p = &ext_i;
-  base_p->a();
-  base_r.a();
-  base_i.a();
+//    string aa = "a";
+//    string &bb = (string &) "a";
+//    string *cc = &((string *) "aaa");
+//    ext ext2(bb);
+//    ext *ext1 = &ext(bb);
+//    ext *ext1 = &ext2;
+//    ext *ext3 = &(*ext1);
+//  ext ext_i(*cc);
+//    base base_i(bb);
+//    base *base_p;
+//  base &base_r = ext_i;
+//  base_p = &ext_i;
+//  base_p->a();
+//  base_r.a();
+//  base_i.a();
 
 //    std::vector<string> vector1{"1", "2", "3"};
 //    vector1.insert(vector1.begin() += 1, "a");
@@ -71,8 +83,10 @@ Java_org_huihui_cpptest_MainActivity_stringFromJNI(
 //    std::deque deque1;
 //    std::list list1;
 //    list1.insert(++list1.begin(), "b");
-  return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF(hello.c_str());
 }
+
+
 
 
 
